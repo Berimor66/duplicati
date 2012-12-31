03747b2ab011521d9056a53782164718cbaa79cc
aaron.hamid@gmail.com <aaron.hamid@gmail.com@59da171f-624f-0410-aa54-27559c288bec>
2012-12-31 07:56:01 +0000

    isse #331 reset TempPath and SignatureCachePath if they are invalid after restore; minor refactorings to support testing; added some basic backup/restore tests
    * FinishedRestoreSetup.cs: issue #331 extracted NormalizeApplicationSettings to reset TempPath and SignatureCachePath if they are invalid after restore
    * Utility.cs: added GetEntryAssembly method to accommodate testing
    * CommunicationStatistics.cs: updated to use Utility.GetEntryAssembly
    * Options.cs: updated to extract DefaultBackupName which defers to Utility.GetEntryAssembly
    * ConsolePasswordInput.cs: circumvent issue where polling infinitely grows string with '\0' values
    * FinishedRestoreSetupTest.cs: tests FinishedRestoreSetup properly normalizes restored settings
    * InterfaceTest.cs: tests basic backup and restore
    * WizardSettingsWrapperTest.cs: just tests how WSW interprets its settings
    * InheritableTestCase.cs, BaseDuplicatiTest.cs, BaseConfigurationTest.cs, BaseBackupTest.cs: base test classes
    * UtilityTest.cs: tests Duplicati.Library.Utility.Utility
    * ProgramTest.cs: some refactoring against base test class
    * Utils.cs: internal test utils
    
    git-svn-id: https://duplicati.googlecode.com/svn/sandboxes/Aaron/issue331@1500 59da171f-624f-0410-aa54-27559c288bec

--- Duplicati_Test/BaseDuplicatiTest.cs
+++ Duplicati_Test/BaseDuplicatiTest.cs
@@ -1,4 +1,3 @@
-#region Disclaimer / License
 //  Copyright (C) 2012, Aaron Hamid
 //  https://github.com/ahamid, aaron.hamid@gmail.com
 //  
@@ -15,48 +14,39 @@
 //  You should have received a copy of the GNU Lesser General Public
 //  License along with this library; if not, write to the Free Software
 //  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-#endregion
 using System;
 using System.Data.LightDatamodel;
+using System.Linq;
 using Duplicati.Library.Utility;
 using Duplicati.Datamodel;
 using Duplicati.Server;
+using Duplicati.Library.Backend;
+using System.Collections.Generic;
 
 namespace Duplicati_Test
 {
     // Base class for Duplicati NUnit tests
-    public abstract class BaseDuplicatiTest
+    public abstract class BaseDuplicatiTest : InheritableTestCase
     {
-        // helper that invokes a closure in the context of a temporary folder
-        protected static void withTempFolder(Action<TempFolder> action)
-        {
-            using(TempFolder tf = new TempFolder())
+        // helper that initializes the Program.DataConnection and invokes a callback
+        protected static void WithProgramDbConnection(TempFolder tf, Action<IDataFetcherWithRelations> action) {
+            using(System.Data.IDbConnection con = Utils.CreateDbConnection())
             {
-                action(tf);
+                Utils.InitializeDbConnection(tf, con);
+                action(Duplicati.GUI.Program.DataConnection);
             }
         }
         
         // helper that invokes a closure with a loaded test Duplicati applications settings database
-        protected static void withApplicationSettingsDb(TempFolder tf, Action<TempFolder, ApplicationSettings> action)
+        protected static void WithApplicationSettingsDb(TempFolder tf, Action<TempFolder, ApplicationSettings> action)
         {
-            using(System.Data.IDbConnection con = (System.Data.IDbConnection)Activator.CreateInstance(Duplicati.Server.SQLiteLoader.SQLiteConnectionType))
-            {
-                Duplicati.GUI.Program.OpenSettingsDatabase(con, tf, "Duplicati_Test.sqlite");
-                
-                var dataFetcher = new DataFetcherWithRelations(new SQLiteDataProvider(con));
+            WithProgramDbConnection(tf, (dataFetcher) => {
                 var appSettings = new ApplicationSettings(dataFetcher);
                 
                 action(tf, appSettings);
                 
                 dataFetcher.CommitRecursive(dataFetcher.GetObjects<ApplicationSetting>());
-            }
-        }
-    
-        // helper that invokes a closure with a new loaded test Duplicati application settings database
-        protected static void withNewApplicationSettingsDb(Action<TempFolder, ApplicationSettings> action)
-        {
-            withTempFolder((tf) => {
-                withApplicationSettingsDb(tf, action);
+
             });
         }
     }
--- Duplicati_Test/ProgramTest.cs
+++ Duplicati_Test/ProgramTest.cs
@@ -1,4 +1,3 @@
-#region Disclaimer / License
 //  Copyright (C) 2012, Aaron Hamid
 //  https://github.com/ahamid, aaron.hamid@gmail.com
 //  
@@ -15,7 +14,6 @@
 //  You should have received a copy of the GNU Lesser General Public
 //  License along with this library; if not, write to the Free Software
 //  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-#endregion
 using System;
 using System.Data.LightDatamodel;
 using NUnit.Framework;
@@ -28,7 +26,7 @@ namespace Duplicati_Test
 {
     // Tests Duplication.GUI.Program
     [TestFixture()]
-	public class ProgramTest : BaseDuplicatiTest
+	public class ProgramTest : BaseConfigurationTest
     {
         // Test initial Program UseDbEncryption flag state
         [Test()]
@@ -39,7 +37,7 @@ namespace Duplicati_Test
         // Tests that the a new application settings db is created if missing, and yields default path values        [Test()]
         public void TestApplicationSettingsInitialization()
         {
-            withNewApplicationSettingsDb((tf, appSettings) => {
+            WithApplicationSettingsDb(this.m_config_dir, (tf, appSettings) => {
                 Assert.IsTrue(System.IO.File.Exists(System.IO.Path.Combine(tf, "Duplicati_Test.sqlite")));
                 Assert.AreEqual(ApplicationSettings.DefaultTempPath, appSettings.TempPath);
                 Assert.AreEqual(ApplicationSettings.DefaultSignatureCachePath, appSettings.SignatureCachePath);
@@ -50,30 +48,29 @@ namespace Duplicati_Test
         [Test()]
         public void TestResetPaths()
         {
-            withTempFolder((tf) => {
-                // set non-default values
-                withApplicationSettingsDb(tf, (_tf, appSettings) => {
-                    Assert.AreEqual(ApplicationSettings.DefaultTempPath, appSettings.TempPath);
-                    Assert.AreEqual(ApplicationSettings.DefaultSignatureCachePath, appSettings.SignatureCachePath);
-                    
-                    appSettings.TempPath= "NewTempPath";
-                    appSettings.SignatureCachePath = "NewSignatureCachePath";
-                });
+            
+            // set non-default values
+            WithApplicationSettingsDb(this.m_config_dir, (tf, appSettings) => {
+                Assert.AreEqual(ApplicationSettings.DefaultTempPath, appSettings.TempPath);
+                Assert.AreEqual(ApplicationSettings.DefaultSignatureCachePath, appSettings.SignatureCachePath);
                 
-                // unset values
-                withApplicationSettingsDb(tf, (_tf, appSettings) => {
-                    Assert.AreEqual("NewTempPath", appSettings.TempPath);
-                    Assert.AreEqual("NewSignatureCachePath", appSettings.SignatureCachePath);
-                    
-                    appSettings.TempPath = "";
-                    appSettings.SignatureCachePath = "";
-                });
+                appSettings.TempPath= "NewTempPath";
+                appSettings.SignatureCachePath = "NewSignatureCachePath";
+            });
+            
+            // unset values
+            WithApplicationSettingsDb(this.m_config_dir, (tf, appSettings) => {
+                Assert.AreEqual("NewTempPath", appSettings.TempPath);
+                Assert.AreEqual("NewSignatureCachePath", appSettings.SignatureCachePath);
                 
-                // check that they are reset to defaults
-                withApplicationSettingsDb(tf, (_tf, appSettings) => {
-                    Assert.AreEqual(ApplicationSettings.DefaultTempPath, appSettings.TempPath);
-                    Assert.AreEqual(ApplicationSettings.DefaultSignatureCachePath, appSettings.SignatureCachePath);
-                });
+                appSettings.TempPath = "";
+                appSettings.SignatureCachePath = "";
+            });
+            
+            // check that they are reset to defaults
+            WithApplicationSettingsDb(this.m_config_dir, (tf, appSettings) => {
+                Assert.AreEqual(ApplicationSettings.DefaultTempPath, appSettings.TempPath);
+                Assert.AreEqual(ApplicationSettings.DefaultSignatureCachePath, appSettings.SignatureCachePath);
             });
         }
     }
